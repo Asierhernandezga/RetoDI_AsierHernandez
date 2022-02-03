8e2094191c7428a29bb0ebecfbed2919f6e21c20
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2022-02-03 09:53:25 +0100

    Hasta el 16 hecho el update no funciona del todo

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -31,9 +31,18 @@ namespace DI_RetoCS
         {
             this.button1 = new System.Windows.Forms.Button();
             this.lblNoConectado = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
             this.lblFacturas = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.buttonCargar = new System.Windows.Forms.Button();
+            this.lblFilasInsertadas = new System.Windows.Forms.Label();
+            this.buttonGuardarXML = new System.Windows.Forms.Button();
+            this.buttonLeerXML = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAñadirX = new System.Windows.Forms.Button();
+            this.buttonQuitarUltimo = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.buttonBorrarDataSet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -56,13 +65,13 @@ namespace DI_RetoCS
             this.lblNoConectado.TabIndex = 1;
             this.lblNoConectado.Text = "No Conectado";
             // 
-            // dataGridView1
+            // DataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 73);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 2;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(16, 73);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(772, 168);
+            this.DataGridView1.TabIndex = 2;
             // 
             // lblFacturas
             // 
@@ -70,22 +79,121 @@ namespace DI_RetoCS
             this.lblFacturas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFacturas.Location = new System.Drawing.Point(13, 44);
             this.lblFacturas.Name = "lblFacturas";
-            this.lblFacturas.Size = new System.Drawing.Size(60, 16);
+            this.lblFacturas.Size = new System.Drawing.Size(99, 16);
             this.lblFacturas.TabIndex = 3;
-            this.lblFacturas.Text = "Facturas";
+            this.lblFacturas.Text = "Tabla Facturas";
+            // 
+            // buttonCargar
+            // 
+            this.buttonCargar.Location = new System.Drawing.Point(154, 31);
+            this.buttonCargar.Name = "buttonCargar";
+            this.buttonCargar.Size = new System.Drawing.Size(61, 29);
+            this.buttonCargar.TabIndex = 22;
+            this.buttonCargar.Text = "CARGAR";
+            this.buttonCargar.UseVisualStyleBackColor = true;
+            this.buttonCargar.Click += new System.EventHandler(this.buttonCargar_Click);
+            // 
+            // lblFilasInsertadas
+            // 
+            this.lblFilasInsertadas.AutoSize = true;
+            this.lblFilasInsertadas.Location = new System.Drawing.Point(221, 39);
+            this.lblFilasInsertadas.Name = "lblFilasInsertadas";
+            this.lblFilasInsertadas.Size = new System.Drawing.Size(87, 13);
+            this.lblFilasInsertadas.TabIndex = 23;
+            this.lblFilasInsertadas.Text = "lblFilasInsertadas";
+            // 
+            // buttonGuardarXML
+            // 
+            this.buttonGuardarXML.Location = new System.Drawing.Point(16, 256);
+            this.buttonGuardarXML.Name = "buttonGuardarXML";
+            this.buttonGuardarXML.Size = new System.Drawing.Size(75, 36);
+            this.buttonGuardarXML.TabIndex = 26;
+            this.buttonGuardarXML.Text = "GUARDAR XML";
+            this.buttonGuardarXML.UseVisualStyleBackColor = true;
+            this.buttonGuardarXML.Click += new System.EventHandler(this.buttonGuardarXML_Click);
+            // 
+            // buttonLeerXML
+            // 
+            this.buttonLeerXML.Location = new System.Drawing.Point(97, 256);
+            this.buttonLeerXML.Name = "buttonLeerXML";
+            this.buttonLeerXML.Size = new System.Drawing.Size(75, 36);
+            this.buttonLeerXML.TabIndex = 28;
+            this.buttonLeerXML.Text = "LEER XML";
+            this.buttonLeerXML.UseVisualStyleBackColor = true;
+            this.buttonLeerXML.Click += new System.EventHandler(this.buttonLeerXML_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(178, 256);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 36);
+            this.buttonUpdate.TabIndex = 29;
+            this.buttonUpdate.Text = "UPDATE";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonAñadirX
+            // 
+            this.buttonAñadirX.Location = new System.Drawing.Point(259, 256);
+            this.buttonAñadirX.Name = "buttonAñadirX";
+            this.buttonAñadirX.Size = new System.Drawing.Size(75, 36);
+            this.buttonAñadirX.TabIndex = 30;
+            this.buttonAñadirX.Text = "Añadir X";
+            this.buttonAñadirX.UseVisualStyleBackColor = true;
+            this.buttonAñadirX.Click += new System.EventHandler(this.buttonAñadirX_Click);
+            // 
+            // buttonQuitarUltimo
+            // 
+            this.buttonQuitarUltimo.Location = new System.Drawing.Point(340, 256);
+            this.buttonQuitarUltimo.Name = "buttonQuitarUltimo";
+            this.buttonQuitarUltimo.Size = new System.Drawing.Size(75, 36);
+            this.buttonQuitarUltimo.TabIndex = 31;
+            this.buttonQuitarUltimo.Text = "Quitar X";
+            this.buttonQuitarUltimo.UseVisualStyleBackColor = true;
+            this.buttonQuitarUltimo.Click += new System.EventHandler(this.buttonQuitarUltimo_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(323, 34);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(454, 20);
+            this.Label1.TabIndex = 32;
+            this.Label1.Text = "Con Dataset/OleDbDataAdapter/OLEDBCOMMANDBUILDER";
+            // 
+            // buttonBorrarDataSet
+            // 
+            this.buttonBorrarDataSet.Location = new System.Drawing.Point(421, 256);
+            this.buttonBorrarDataSet.Name = "buttonBorrarDataSet";
+            this.buttonBorrarDataSet.Size = new System.Drawing.Size(75, 36);
+            this.buttonBorrarDataSet.TabIndex = 33;
+            this.buttonBorrarDataSet.Text = "Borrar DATASET";
+            this.buttonBorrarDataSet.UseVisualStyleBackColor = true;
+            this.buttonBorrarDataSet.Click += new System.EventHandler(this.buttonBorrarDataSet_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonBorrarDataSet);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.buttonQuitarUltimo);
+            this.Controls.Add(this.buttonAñadirX);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonLeerXML);
+            this.Controls.Add(this.buttonGuardarXML);
+            this.Controls.Add(this.lblFilasInsertadas);
+            this.Controls.Add(this.buttonCargar);
             this.Controls.Add(this.lblFacturas);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DataGridView1);
             this.Controls.Add(this.lblNoConectado);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,7 +203,16 @@ namespace DI_RetoCS
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblNoConectado;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DataGridView1;
         private System.Windows.Forms.Label lblFacturas;
+        private System.Windows.Forms.Button buttonCargar;
+        private System.Windows.Forms.Label lblFilasInsertadas;
+        private System.Windows.Forms.Button buttonGuardarXML;
+        private System.Windows.Forms.Button buttonLeerXML;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAñadirX;
+        private System.Windows.Forms.Button buttonQuitarUltimo;
+        internal System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button buttonBorrarDataSet;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient; //añadido
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic; //añadido
 
 namespace DI_RetoCS
 {
@@ -16,10 +18,97 @@ namespace DI_RetoCS
         {
             InitializeComponent();
         }
+        // NO CONECTADO (Tiene DATASET Y SQLDATAADAPTER)
+        private string sql;
+        private MiConexion Conexion = new MiConexion();
+        private DataSet das1;
+        private SqlDataAdapter adap1;
 
         private void button1_Click(object sender, EventArgs e)
         {
             Padre.f1.Activate();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            sql = "select * from Vendedores";
+            SqlCommand cmd1 = new SqlCommand(sql, Conexion.pConexion);
+            adap1 = new SqlDataAdapter(cmd1);
+            das1 = new DataSet();
+            adap1.Fill(das1, "aaa");
+            DataGridView1.DataSource = das1.Tables[0];
+            DataGridView1.Columns[0].Width = 60;
+            DataGridView1.Columns[1].Width = 160;
+            DataGridView1.Columns[2].Width = 80;
+            DataGridView1.Columns[3].Width = 100;
+            DataGridView1.Columns[4].Width = 100;
+            DataGridView1.Columns[5].Width = 100;
+            DataGridView1.Columns[6].Width = 60;
+            DataGridView1.Columns[7].Width = 60;
+            DataGridView1.Columns[8].Width = 60;
+
+            SqlCommandBuilder cmb = new SqlCommandBuilder(adap1); //NUEVO
+            //Para que me cree las sentencias UPDATE, DELETE, INSERT
+            //MessageBox.Show(cmb.GetInsertCommand().CommandText);
+            //Lo que genera el cmb lo utiliza el adap1.Update(das1, "aaa");
+        }
+
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+            das1.Tables[0].Clear();
+            adap1.Fill(das1, "aaa");
+            lblFilasInsertadas.Text = das1.Tables[0].Rows.Count + " filas.";
+        }
+
+        private void buttonGuardarXML_Click(object sender, EventArgs e)
+        {
+            //GUARDAR XML
+            das1.WriteXml("bbb.xml"); // se guarda en el \bin\debug
+            das1.WriteXmlSchema("ccc.xml");
+        }
+
+        private void buttonLeerXML_Click(object sender, EventArgs e)
+        {
+            //leer XML
+            das1.Tables[0].Clear();
+            das1.ReadXml("bbb.xml"); //en el \bin\debug
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                adap1.Update(das1, "aaa");//ACTUALIZA LA BD
+                //PRIMERO HAY QUE PONER EL COMMANDBUILDER.
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show(EX.ToString());
+            }
+        }
+
+        private void buttonAñadirX_Click(object sender, EventArgs e)
+        {
+            //añadir x
+            das1.Tables[0].Rows[0][3] += "X";
+        }
+
+        private void buttonQuitarUltimo_Click(object sender, EventArgs e)
+        {
+            //quitar el último caracter
+            string aaa = Convert.ToString(das1.Tables[0].Rows[0][3]);
+            das1.Tables[0].Rows[0][3] = aaa.Substring(0, aaa.Length - 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBorrarDataSet_Click(object sender, EventArgs e)
+        {
+            //BORRAR DATASET
+            das1.Tables[0].Clear();
         }
     }
 }
