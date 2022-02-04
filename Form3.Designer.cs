e8e8350ae7728c788b3f498d86d1459c0da0d180
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2022-02-04 12:50:24 +0100

    23 hecho

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -30,11 +30,22 @@ namespace DI_RetoCS
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.LabelClientesFactura = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.lblTablaFactura = new System.Windows.Forms.Label();
+            this.lblTablaClientes = new System.Windows.Forms.Label();
+            this.DataGridView2 = new System.Windows.Forms.DataGridView();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(713, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -42,20 +53,124 @@ namespace DI_RetoCS
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // LabelClientesFactura
+            // 
+            this.LabelClientesFactura.AutoSize = true;
+            this.LabelClientesFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelClientesFactura.Location = new System.Drawing.Point(12, 12);
+            this.LabelClientesFactura.Name = "LabelClientesFactura";
+            this.LabelClientesFactura.Size = new System.Drawing.Size(297, 24);
+            this.LabelClientesFactura.TabIndex = 11;
+            this.LabelClientesFactura.Text = "Relaciones Tabla Clientes-Factura";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(270, 257);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(39, 13);
+            this.Label6.TabIndex = 27;
+            this.Label6.Text = "Label6";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(164, 257);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(39, 13);
+            this.Label5.TabIndex = 26;
+            this.Label5.Text = "Label5";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(151, 69);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(39, 13);
+            this.Label4.TabIndex = 25;
+            this.Label4.Text = "Label4";
+            // 
+            // lblTablaFactura
+            // 
+            this.lblTablaFactura.AutoSize = true;
+            this.lblTablaFactura.Location = new System.Drawing.Point(12, 260);
+            this.lblTablaFactura.Name = "lblTablaFactura";
+            this.lblTablaFactura.Size = new System.Drawing.Size(73, 13);
+            this.lblTablaFactura.TabIndex = 23;
+            this.lblTablaFactura.Text = "Tabla Factura";
+            // 
+            // lblTablaClientes
+            // 
+            this.lblTablaClientes.AutoSize = true;
+            this.lblTablaClientes.Location = new System.Drawing.Point(12, 70);
+            this.lblTablaClientes.Name = "lblTablaClientes";
+            this.lblTablaClientes.Size = new System.Drawing.Size(74, 13);
+            this.lblTablaClientes.TabIndex = 22;
+            this.lblTablaClientes.Text = "Tabla Clientes";
+            // 
+            // DataGridView2
+            // 
+            this.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView2.Location = new System.Drawing.Point(12, 276);
+            this.DataGridView2.Name = "DataGridView2";
+            this.DataGridView2.Size = new System.Drawing.Size(573, 150);
+            this.DataGridView2.TabIndex = 21;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(12, 89);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(449, 150);
+            this.DataGridView1.TabIndex = 20;
+            this.DataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(528, 136);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEliminar.TabIndex = 28;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.lblTablaFactura);
+            this.Controls.Add(this.lblTablaClientes);
+            this.Controls.Add(this.DataGridView2);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.LabelClientesFactura);
             this.Controls.Add(this.button1);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        internal System.Windows.Forms.Label LabelClientesFactura;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label lblTablaFactura;
+        internal System.Windows.Forms.Label lblTablaClientes;
+        internal System.Windows.Forms.DataGridView DataGridView2;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.Button buttonEliminar;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,10 +18,83 @@ namespace DI_RetoCS
         {
             InitializeComponent();
         }
-
+        private MiConexion Conexion = new MiConexion();
+        DataSet das1 = new DataSet();
+        SqlDataAdapter adaporders;
+        SqlDataAdapter adapdetails;
         private void button1_Click(object sender, EventArgs e)
         {
             Padre.f1.Activate();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql2 = "select * from Clientes";
+                string sql1 = "select * from Facturas";
+                //'Hay un solo das1 y dos adaptadores
+                adaporders = new SqlDataAdapter(sql2, Conexion.pConexion);
+                adaporders.Fill(das1, "padre");
+                adapdetails = new SqlDataAdapter(sql1, Conexion.pConexion);
+                adapdetails.Fill(das1, "hijo");
+
+                //'Creo la relacion en el das1 
+                DataColumn a, b;
+                a = das1.Tables["padre"].Columns["CodCli"];
+                b = das1.Tables["hijo"].Columns["CodCli"];
+                das1.Relations.Add("mirelacion", a, b);
+                //'visualizamos
+                DataGridView1.DataSource = das1;
+                DataGridView1.DataMember = "padre";
+                DataGridView2.DataSource = das1;
+                DataGridView2.DataMember = "padre.mirelacion";
+
+                //'crear automaticamente la sql para update -
+                SqlCommandBuilder cmb1 = new SqlCommandBuilder(adaporders);
+                SqlCommandBuilder cmb2 = new SqlCommandBuilder(adapdetails);
+
+                Label4.Text = Convert.ToString(das1.Tables["padre"].Rows.Count - 1) + " filas.";
+                Label5.Text = Convert.ToString(DataGridView2.Rows.Count - 1) + " filas.";
+
+                das1.Tables[0].PrimaryKey = new DataColumn[] { das1.Tables[0].Columns["CodCli"] };
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //'update
+            adaporders.Update(das1, "padre");
+            adapdetails.Update(das1, "hijo");
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //'Creo una clave primaria para poder utilizar el FIND 
+            das1.Tables["padre"].PrimaryKey = new DataColumn[] { das1.Tables["padre"].Columns["orderid"] };
+            int aaa = 0;
+            // AGREGO LA REFERENCIA MICROSOFT.VISUALBASIC
+            aaa = Convert.ToInt32(Interaction.InputBox("Escribe el CodCli", "Busqueda por CodCli", "102"));
+            DataRow drw = das1.Tables["padre"].Rows.Find(aaa);
+            MessageBox.Show(drw.ItemArray[1].ToString());
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Label5.Text = Convert.ToString(DataGridView2.Rows.Count - 1) + " filas.";
+        }
+
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            int aaa = 0;
+            aaa = Convert.ToInt32(Interaction.InputBox("Escribe el CodCli", "Borrar por CodCli", "102"));
+            das1.Tables[0].Rows.Find(aaa).Delete();
         }
     }
 }
